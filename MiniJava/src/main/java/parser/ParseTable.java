921bcc5c17bdc669dcb59687c19ea55a9a589a3f
alimohammadiamirhossein <alimohammadiamirhossein@gmail.com>
2023-01-05 16:33:49 +0330

    Loose Coupling!

--- MiniJava/src/main/java/parser/ParseTable.java
+++ MiniJava/src/main/java/parser/ParseTable.java
@@ -5,14 +5,15 @@ import scanner.token.Token;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 /**
  * Created by mohammad hosein on 6/25/2015.
  */
 
 public class ParseTable {
-    private ArrayList<Map<Token, Action>> actionTable;
-    private ArrayList<Map<NonTerminal, Integer>> gotoTable;
+    private List<Map<Token, Action>> actionTable;
+    private List<Map<NonTerminal, Integer>> gotoTable;
 
     public ParseTable(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
--- MiniJava/src/main/java/semantic/symbol/SymbolTable.java
+++ MiniJava/src/main/java/semantic/symbol/SymbolTable.java
@@ -8,6 +8,7 @@ import errorHandler.ErrorHandler;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SymbolTable {
@@ -145,7 +146,7 @@ public class SymbolTable {
         public int codeAddress;
         public Map<String, Symbol> parameters;
         public Map<String, Symbol> localVariable;
-        private ArrayList<String> orderdParameters;
+        private List<String> orderdParameters;
         public int callerAddress;
         public int returnAddress;
         public SymbolType returnType;
